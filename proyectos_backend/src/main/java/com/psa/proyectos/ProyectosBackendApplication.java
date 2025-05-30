b8b86cba0846d05a3b6b618dc762c73bf01d6d41
Valentino Carmona <vcarmona@fi.uba.ar>
2025-05-30 12:39:14 -0300

    feat: add initial backend

---
+++ proyectos_backend/src/main/java/com/psa/proyectos/ProyectosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.psa.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectosBackendApplication.class, args);
+	}
+
+}
